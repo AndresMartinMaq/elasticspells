7dc0016a1e988f464a0d369c7697095e6d0fa05f
Andres Martin <andres@beapplied.com>
2022-08-19 13:24:24 +0100

    makes it use store data

--- src/components/SpellCard/SpellCard.vue
+++ src/components/SpellCard/SpellCard.vue
@@ -6,21 +6,10 @@
       <span class="inlineTitle">Range:</span><span> {{ range }}ft</span>
     </p>
     <p><span class="inlineTitle">Componenets:</span> {{ components }}</p>
-    <p>{{ description }}</p>
     <hr />
     <div>
-      <p>
-        A storm made up of sheets of roaring flame appears in a location you
-        choose within range. The area of the storm consists of up to ten 10-foot
-        cubes, which you can arrange as you wish. Each cube must have at least
-        one face adjacent to the face of another cube. Each creature in the area
-        must make a Dexterity saving throw. It takes 7d10 fire damage on a
-        failed save, or half as much damage on a successful one.
-      </p>
-      <p>
-        The fire damages objects in the area and ignites flammable objects that
-        aren't being worn or carried. If you choose, plant life in the area is
-        unaffected by this spell.
+      <p v-for="(paragraph, index) in description" :key="index">
+        {{ paragraph }}
       </p>
     </div>
   </div>
@@ -46,7 +35,10 @@ export default {
       type: String,
       default: "V, S, C",
     },
-    description: String,
+    description: {
+      type: Array,
+      required: true,
+    },
   },
 };
 </script>
--- src/pages/SpellExplorer.vue
+++ src/pages/SpellExplorer.vue
@@ -4,27 +4,47 @@
     <SearchBar />
     <div class="spellsArea">
       <SpellCard
-        spellName="Fire Storm"
-        castingTime="1 actioon"
-        :range="150"
-        components="V, S"
+        v-for="s in spells"
+        :key="s.name"
+        :spellName="s.name"
+        :castingTime="getCastingTime(s)"
+        :range="s.range.distance.amount"
+        :components="getComponentsString(s)"
+        :description="s.entries"
       />
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 import SearchBar from "@/components/SearchBar/SearchBar.vue";
 import SpellCard from "@/components/SpellCard/SpellCard.vue";
 
 export default {
-  name: "HomeView",
+  name: "SpellExplorer",
   components: {
     SearchBar,
     SpellCard,
   },
   mounted() {
     this.$store.dispatch("getSpells");
+  },
+  computed: {
+    ...mapState(["spells"]),
+  },
+  methods: {
+    getCastingTime(spell) {
+      return `${spell.time[0].number} ${spell.time[0].unit}`;
+    },
+    getComponentsString({ components }) {
+      // Could make this fancier with spaces and commas, but not the point of this project.
+      return (
+        (components.v ? "V" : "") +
+        (components.s ? "S" : "") +
+        (components.m ? "M" : "")
+      );
+    },
   },
 };
 </script>
