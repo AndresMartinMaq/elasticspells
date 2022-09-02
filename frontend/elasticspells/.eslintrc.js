e9e012b98a6768650a412c645bc4956214ec54b7
Andres Martin <andres@beapplied.com>
2022-09-02 10:47:00 +0100

    moving files

--- frontend/elasticspells/.eslintrc.js
+++ frontend/elasticspells/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "plugin:prettier/recommended",
+    //"@vue/prettier", TODO figure out how to add this
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
@@ -14,6 +15,11 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "max-len": [2, 150, 4, { ignoreStrings: true, ignoreUrls: true }],
+    "import/no-unresolved": [0, { caseSensitive: false }],
+    //'prettier/prettier': ['error', { singleQuote: true }],
+    //quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    //semi: ["error", "never"],
   },
   overrides: [
     {
