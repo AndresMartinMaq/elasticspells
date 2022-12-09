e82386791bb405dfa5a01a387e64cc345930ddcd
Andres Martin <andres@beapplied.com>
2022-12-09 13:51:19 +0000

    adds 2nd aggregation, uses non-nested nested field

--- backend/logic/spell_aggregations.js
+++ backend/logic/spell_aggregations.js
@@ -48,14 +48,56 @@ exports.numberOfSpellsOfEachLevel = async (req, res, next) => {
     return result
 }
 
+/** How many spells are available to each DnD Class?
+ * Interesting here is the querying of a fiend in an object structure, which is possible even if
+ * the object is not in a nested-type mapping due to the way Elasticsearch flattens object fields
+ * and the fact that in this particular usecase we do not need to differentiate between the inner
+ * objects - we just care about whether they contain the DnD-class name.
+ * https://www.elastic.co/guide/en/elasticsearch/reference/current/nested.html 
+ * If I needed a different sort of query that DID requre differentiating between these objects,
+ * I would need to make the "fromClassList" a nested mapping type (see below).
+*/
+exports.getNumberOfSpellsPerDnDClass = async (req, res, next) => {
+    console.log(`Querying for NumberOfSpellsPerDnDClass`)
+
+    const esResponse = await esClient.search({
+        index: 'elasticspells_spells',
+        body: {
+            query: {
+                query_string: {
+                    query: "*"
+                },
+            },
+            size: 0,
+            aggs: {
+                classes: {
+                    terms: { field: "classes.fromClassList.name.keyword" }
+                },
+            },
+        }
+    })
+
+    const result = esResponse.aggregations.classes.buckets.map( (bucket) => {
+        return { 
+            DndClass: bucket.key, 
+            numberOfSpells: bucket.doc_count 
+        }
+    })
+
+    console.log(`Spells of each DnD Class: ${JSON.stringify(result)}`)
+
+    return result
+}
+
+
 // Example complex aggregation - How many (of these) spells are available to each DnD Class?
 // nested terms query
 // It seems like this will not work right now because the "classes" field is not an object in the mapping, but a string. I think.
 // [parsing_exception] Unknown key for a VALUE_STRING in [fromClassList]: [field].
 exports.getNumberOfSpellsPerDnDClass = async (req, res, next) => {
-    const searchTerm = req.query.searchTerm
+    let searchTerm = req.query.searchTerm
 
-    if (!searchTerm)
+    if (!searchTerm || searchTerm === 'undefined')
         searchTerm = "*"
 
     console.log(`Querying for NumberOfSpellsPerDnDClass`)
@@ -93,15 +135,5 @@ exports.getNumberOfSpellsPerDnDClass = async (req, res, next) => {
         }
     })
 
-    console.log(`Searched for ${searchTerm} - found ${esResponse.hits.total} results.`)
-
-    console.log(`Raw response from ES: ${JSON.stringify(esResponse)}`)
-
-
-    // TODO is this right?
-    const spellsData = esResponse.hits.hits.map(esHit => {
-        return esHit._source
-    })
-
-    return spellsData
+    return result
 }
